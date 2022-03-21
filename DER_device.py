1dfc42f0e1a39a8d16a6827f93ab08bdee083447
Calvin Long <csclong@gmail.com>
2022-03-21 17:11:27 +1100

    Fixed up connection between DER_devices and DER_master

--- DER_device.py
+++ DER_device.py
@@ -1,18 +1,53 @@
+'''
+Created by Calvin Long z5255352 (UNSW)
+How to use DER_device.py
+
+Start a DER_master server to accept all the connections
+
+then run:
+
+python3 DER_device.py Address_of_server Port_of_server number_of_devices
+
+e.g.
+
+python3 DER_device.py 127.0.0.1 1234 10
+
+This will automatically connect 10 der devices to DER_master server running
+'''
+
+
 from socket import *
 import sys
 from threading import Thread
+import time
 
 class ClientThread(Thread):
-    def __init__(self, clientAddress, clientSocket):
+    def __init__(self, clientSocket, serverAddress, client_num):
         Thread.__init__(self)
-        self.clientAddress = clientAddress
         self.clientSocket = clientSocket
+        self.clientSocket.connect(serverAddress)
         self.clientAlive = True
-        
+        self.client_num = client_num
+
     def run(self):
-        pass
+        while self.clientAlive:
+            time.sleep(5)
+            self.communicate('Alive!!!')
+
+        self.clientSocket.close()
 
     def communicate(self, message):
-        self.clientSocket.send(message.encode())
+        self.clientSocket.send(('Client '+ str(self.client_num)+ ': '+ message).encode())
 
-    
+    def display_info(self, info):
+        print('Client ' + str(self.client_num) + ': ' + info)
+
+serverHost = sys.argv[1]
+serverPort = int(sys.argv[2])
+number_devices = int(sys.argv[3])
+
+serverAddress = (serverHost, serverPort)
+
+for i in range(0, number_devices):
+    client_com = ClientThread(socket(AF_INET, SOCK_STREAM), serverAddress, i)
+    client_com.start()
--- DER_master.py
+++ DER_master.py
@@ -1,11 +1,23 @@
+'''
+Created by Calvin Long z5255352 (UNSW)
+
+How to use DER_master.py
+
+python3 DER_master.py ip_address Server_port
+
+e.g.
+
+python3 DER_master.py 127.0.0.1 8081
+
+This code will wait for in comming connections and display the data obtained from the Devices 
+'''
+
 from socket import *
 from threading import Thread
 import sys
 
-serverHost = "127.0.0.1"
-serverPort = int(sys.argv[1])
-block_duration = int(sys.argv[2])
-client_timeout = int(sys.argv[3])
+serverHost = sys.argv[1]
+serverPort = int(sys.argv[2])
 serverAddress = (serverHost, serverPort)
 
 # define socket for the server side and bind address
@@ -18,15 +30,34 @@ class ClientThread(Thread):
     def __init__(self, clientAddress, clientSocket):
         Thread.__init__(self)
         clients.append(self)
+        self.clientAddress = clientAddress
+        self.clientSocket = clientSocket
+        self.clientAlive = True
+        
 
     def run(self):
-        pass
+        data = ''
+        while self.clientAlive:
+            try:
+                data = self.read_data()
+            except timeout as e:
+                continue
+            
+            if data == '':
+                self.clientAlive = False
+                break
 
-    def rec_data(self):
-        pass
+            self.display_data(data)
+        clients.remove(self)
 
-    def display_data(self):
-        pass
+    def display_data(self, data):
+        print(data)
+
+    def read_data(self):
+        data = self.clientSocket.recv(1024)
+        data = data.decode()
+        data = data.rstrip('\n')
+        return data
 
 print("\n===== Server is running =====")
 print("===== Waiting for connection request from clients...=====")
